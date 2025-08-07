7c267a73b86f4b1694fd6fa8621ba24ededae6f3
localfossa <74490107+localfossa@users.noreply.github.com>
2025-08-07 23:08:43 +0300

    Add files via upload

---
+++ chrome/ARCHIVED/scrap1.js
@@ -0,0 +1,18 @@
+            /*let maxArea = width*height
+            let area = 0;
+
+            let index = 0;
+            const pixelSize = 4;
+
+            while (index < imgData.length) {
+                const pixel = imgData.slice(index, index + pixelSize);
+                index += pixelSize;
+
+                if (pixel[3] != 0) {
+                    area++;
+                };
+            };
+
+            if (area/maxArea > 0.82) {
+                img.style.borderRadius = "0%";
+            };*/
